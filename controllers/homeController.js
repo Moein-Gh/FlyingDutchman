8b6518fbbfed1deac598500427fd668ea56fd35d
moein ghorbanali <moeinghorbanali.biz@gmail.com>
2024-02-16 19:49:40 +0100

    Remove unused imports and duplicate CSS styles

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,5 @@
-// import { Model } from '../models/bearModel.js';
 import { renderHome } from '../views/homeView.js';
 
-// Controller for the Home view
 export function HomeController() {
 	renderHome();
 }
--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,7 +1,6 @@
 import Beer from '../models/bearModel.js';
 import { renderMenu } from '../views/menuView.js';
 
-// Controller for the Menu view
 export async function MenuController() {
 	let beerModel = new Beer();
 	await beerModel.initialize();

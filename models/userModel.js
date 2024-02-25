693c2856234199cc9279516d1f6b5dff98b9a413
moein ghorbanali <moeinghorbanali.biz@gmail.com>
2024-02-25 22:14:14 +0100

    Add user login functionality

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+import { getDataFromSessionStorage } from '../utils.js';
+
+export default class User {
+	constructor() {
+		this.key = 'users';
+	}
+
+	async initialize() {
+		this.data = await getDataFromSessionStorage(this.key);
+	}
+
+	getUser(nr) {
+		if (!this.data) {
+			return null;
+		}
+		return this.data.find((beer) => beer.nr === nr);
+	}
+
+	getUsers(config = {}) {
+		if (!this.data) {
+			return [];
+		} else if (config?.limit) {
+			return this.data.slice(0, config.limit);
+		} else {
+			return this.data;
+		}
+	}
+}
--- views/loginView.js
+++ views/loginView.js
@@ -2,22 +2,24 @@ export function renderLogin(data) {
 	const appContainer = document.getElementById('app');
 
 	appContainer.innerHTML = `
-		<div class="container">
-			<form id="login-form">
-				<input
-					type="text"
-					name="username"
-					id="username-field"
-					class="login-form-field"
-					placeholder="Username" />
-				<input
-					type="password"
-					name="password"
-					id="password-field"
-					class="login-form-field"
-					placeholder="Password" />
-				<input type="submit" value="Login" id="login-form-submit" />
-			</form>
-		</div>
+		<h1>Flying Dutchman Pub</h1>
+		<h2>Please sign in to your existing account</h2>
+		<form action="/login" method="post">
+			<label for="username">Username:</label>
+			<input type="text" id="username" name="username" required />
+			<br />
+			<label for="password">Password:</label>
+			<input type="password" id="password" name="password" required />
+			<br />
+			<a href="/forgot-password">Forgot password?</a>
+			<br />
+			<button type="submit">Login</button>
+		</form>
+		<p>OR</p>
+		<button
+			class="checkout-button"
+			onclick="location.href='/checkout-without-login'">
+			Checkout without login
+		</button>
 	`;
 }

b750b8860d15575c5ef72ab12a1d54d9b941f514
moein ghorbanali <moeinghorbanali.biz@gmail.com>
2024-02-26 11:28:53 +0100

    Fix import statements and remove unnecessary code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import UserModel from '../models/userModel.js';
-=======
+
 import LoginModel from '../models/userModel.js';
->>>>>>> ef5ec56f59d11c1bd887c0901588465f96c931bc
 import { renderLogin } from '../views/loginView.js';
 
 export async function userController() {
@@ -14,30 +12,9 @@ export async function userController() {
 
 	renderLogin();
 
-<<<<<<< HEAD
 	// const loginButton = document.getElementById('login-form');
 	// loginButton.addEventListener('submit', (e) => {
 	// 	e.preventDefault();
 	// 	userModel.login();
 	// });
-=======
-	const loginButton = document.getElementById('login-button');
-	const checkouwithoutloginButton = document.getElementById('checkout-without-login-button');
-	const checkoutbutton = document.getElementById('checkout-button');
-
-	loginButton.addEventListener('submit', (e) => {
-		e.preventDefault();
-		if(loginModel.login(e.target.username.value, e.target.password.value)){
-			location.href = '/menu';
-		
-		};
-	});
-//send to the menu page, no user is necessary for this button.	
-	checkouwithoutloginButton.addEventListener('click', (e) => {
-		location.href = '/menu';
-	});
-	checkoutbutton.addEventListener('click', (e) => {
-		location.href = '/menu';
-	});
->>>>>>> ef5ec56f59d11c1bd887c0901588465f96c931bc
 }

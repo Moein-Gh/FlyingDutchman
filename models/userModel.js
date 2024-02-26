dae8a337330144eaf7129d073d6520d31c622f67
ogersoy <oguzhan.ersoy.9530@student.uu.se>
2024-02-26 06:17:18 +0100

    Login chanegs
    Login now returns 1 for admin and 2 for VIP user.
    Fixed the user mvc's

--- models/userModel.js
+++ models/userModel.js
@@ -8,7 +8,7 @@ export default class User {
 	async initialize() {
 		this.data = await getDataFromSessionStorage(this.key);
 	}
-
+//Fix, should look for user id, not beer id.
 	getUser(nr) {
 		if (!this.data) {
 			return null;
@@ -23,6 +23,32 @@ export default class User {
 			return this.data.slice(0, config.limit);
 		} else {
 			return this.data;
+		}
+	}
+	validate(username, password) {
+		if (!this.data) {
+			return false;
+		}
+		const user = this.data.find((user) => user.username === username);
+		if (!user) {
+			return false;
+		}
+		return user.password === password;
+	}
+	login(username, password) {
+		if (!this.data) {//if there is no data
+			return false;
+		}
+		const user = this.data.find((user) => user.username === username);
+		if (!user) {//if user is not found
+			return false;
+		}
+
+		if(this.data.find((user) => user.credentials === 0) && this.data.find((user) => user.password === password)){
+			return 1;//admin
+		}
+		else{
+			return 2;//user
 		}
 	}
 }
